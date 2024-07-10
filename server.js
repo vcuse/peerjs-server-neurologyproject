1f61ae39a7b24c103ef55e28d2f9ae240b04ea8e
Adam Hussein <adamhusse818@gmail.com>
2024-07-10 14:19:46 -0400

    Update server to allow discovery

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { ExpressPeerServer } = require("peer");
+const app = express();
+
+app.enable("trust proxy");
+
+const PORT = process.env.PORT || 9000;
+const server = app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+    console.log("Press Ctrl+C to quit.");
+});
+
+const peerServer = ExpressPeerServer(server, {
+    path: "/",
+    allow_discovery: true,
+});
+
+app.use("/", peerServer);
+
+module.exports = app;
