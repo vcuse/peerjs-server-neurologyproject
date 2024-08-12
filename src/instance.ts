98af3e146f5450759825084ca27c7f6b56dd9157
David <ferrufinoda2@vcu.edu>
2024-08-12 12:29:26 -0700

    - Added working iOS notifications
    This push adds working ios notifs, however it is missing the certificate for security reasons

--- src/instance.ts
+++ src/instance.ts
@@ -1,4 +1,5 @@
 import type express from "express";
+import apn from 'node-apn';
 import type { Server as HttpServer } from "node:http";
 import type { Server as HttpsServer } from "node:https";
 import path from "node:path";
@@ -39,6 +40,11 @@ export const createInstance = ({
 	const realm: IRealm = new Realm();
 	const messageHandler = new MessageHandler(realm);
 
+    const certKeyPath =  "/Users/ferrufinoda2/Documents/GitHub/peerjs-server-neurologyproject/src/certs/AuthKey_9T5T6LGDWB.p8";
+
+
+
+
 	const api = Api({ config, realm, corsOptions: options.corsOptions });
 	const messagesExpire: IMessagesExpire = new MessagesExpire({
 		realm,
@@ -53,7 +59,8 @@ export const createInstance = ({
 		},
 	});
 
-	app.use(options.path, api);
+    var apn = require('@parse/node-apn');
+
 
 	//use mountpath for WS server
 	const customConfig = {
@@ -61,11 +68,44 @@ export const createInstance = ({
 		path: path.posix.join(app.path(), options.path, "/"),
 	};
 
+    var apnOptions = {
+          token: {
+            key: certKeyPath,
+            keyId: "9T5T6LGDWB",
+            teamId: "VL7R24L9ZQ"
+          },
+          production: false
+        };
+        var apnProvider = new apn.Provider(apnOptions);
+
+    app.use(options.path, api);
+
+        console.log(
+        		"created apn",
+
+        	);
+
 	const wss: IWebSocketServer = new WebSocketServer({
 		server,
 		realm,
 		config: customConfig,
 	});
+
+
+    var note = new apn.Notification();
+
+    note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
+    note.badge = 3;
+    note.sound = "ping.aiff";
+    note.alert = "\uD83D\uDCE7 \u2709 You have a new message";
+    note.payload = {'messageFrom': 'John Appleseed'};
+    note.topic = "vcuse.Neuro-App";
+    apnProvider.send(note, deviceToken).then( (response) => {
+            		// response.sent: Array of device tokens to which the notification was sent succesfully
+            		// response.failed: Array of objects containing the device token (`device`) and either an `error`, or a `status` and `response` from the API
+            console.log("Sent Notificaiton to iphone", response, JSON.stringify(response, null, 2));
+            });
+
 
 	wss.on("connection", (client: IClient) => {
 		const messageQueue = realm.getMessageQueueById(client.getId());
@@ -83,6 +123,9 @@ export const createInstance = ({
 	});
 
 	wss.on("message", (client: IClient, message: IMessage) => {
+
+
+
 		app.emit("message", client, message);
 		messageHandler.handle(client, message);
 	});
